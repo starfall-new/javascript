9faf837670f648be8b45d4b43b49bd86c6f37c59
= <=>
2026-04-22 18:28:08 +0700

    javascript

--- trapesium.js
+++ trapesium.js
@@ -4,7 +4,7 @@ let sisi2 = Number(prompt('masukan sisi b : '))
 let sisi3 = Number(prompt('masukan sisi c : '))
 let sisi4 = Number(prompt('masukan sisi d : '))
 let tinggi = Number(prompt('masukan tinggi : '))
-let alas = 0.5 * (sisi1 + sisi2) * tinggi
+let luas = 0.5 * (sisi1 + sisi2) * tinggi
 let keliling = sisi1 + sisi2 + sisi3 + sisi4
 
 console.log('===============')
@@ -12,5 +12,5 @@ console.log('   H A S I L   ')
 console.log('---------------')
 
 console.log(
-    `luas trapesium\t: ${luas.toFixed(2)} cm
-    keliling trapesium\t: ${keliling.toFixed(2)} cm`)
+    `luas\t\t: ${luas.toFixed(2)} cm
+    keliling\t: ${keliling.toFixed(2)} cm `)

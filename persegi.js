90a77b2b3854ae75f5f01848a70fbddfccfa3da5
= <=>
2026-05-03 19:55:25 +0700

    javascript

--- persegi.js
+++ persegi.js
@@ -5,7 +5,7 @@ let luas = Math.pow(sisi, 2)
 let keliling = 4 * sisi
 console.log('========================')
 console.log('        H A S I L       ')
-console.log('------------------------node ')
+console.log('------------------------')
 
 console.log(
     `luas persegi\t\t: ${luas.toFixed(2) } cm

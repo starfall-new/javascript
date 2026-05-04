edb41a6d9fab11af7797d5646fa374bf17c74a45
= <=>
2026-05-04 21:18:00 +0700

    javascript

---
+++ soal21/soal14.js
@@ -0,0 +1,9 @@
+let Nilai = Number(prompt('Masukan Nilai 1-100'))
+
+if (Nilai >= 100) {
+    console.log('kamu dapat nilai sempurna')
+} else if (Nilai >=75) {
+    console.log('yey di atas kkm')
+} else {
+    console.log('coba lagi yahh')
+}
---
+++ soal21/soal15.js
@@ -0,0 +1,7 @@
+let angka = Number(prompt('Masukan 5 angka :'))
+
+if (angka = 78821) {
+    console.log('You are authenticated')
+} else {
+    console.log('You have no access')
+}

c03094cdf0dfe6b8ad9651b8d90603454f3c9da6
= <=>
2026-06-18 09:59:10 +0700

    javascript

---
+++ algoritma/algo1.js
@@ -0,0 +1,16 @@
+let detik = Number(prompt("Masukank berapa detik"))
+let menit = detik / 60;
+let jam = menit / 60;
+let hari = jam / 24;
+
+if ( jam >= 24){
+    console.log("Totalnya adalah: " + Math.floor(hari) + " hari")
+} else if ( menit >= 60){
+    console.log("Totalnya adalah: " + Math.floor(jam) + " jam")
+} else if ( detik >= 60){
+    console.log("Totalnya adalah: " + Math.floor(menit) + "menit")
+} else if ( detik <= 60){
+    console.log("Totalnya adalah : " + Math.floor(detik) + "detik" )
+} else {
+    console.log("null")
+}

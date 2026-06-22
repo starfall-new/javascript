f2425badf0a25323dc15db7c5e198a61f310e949
= <=>
2026-06-22 18:53:35 +0700

    javascript

---
+++ algoritma/algo11.js
@@ -0,0 +1,7 @@
+let tahun = Number(prompt("Masukan tahun nya"))
+
+if ( tahun % 4 == 0){
+    console.log(tahun + " ini tahun kabisat")
+} else {
+    console.log(tahun + " bukan tahun kabisat")
+}
---
+++ algoritma/algo12.js
@@ -0,0 +1,7 @@
+let tahun = Number(prompt("Masukkan tahun masehi "))
+
+if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
+    console.log(tahun + " ini tahun kabisat (29 februari)")
+} else {
+    console.log(tahun + " bukan tahun kabisat (28 februari)")
+}
